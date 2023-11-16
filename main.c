da9b4c583b960df3ac6f7a0a6e52d72b57b89757
s25y25n <144671761+s25y25n@users.noreply.github.com>
2023-11-16 15:34:47 +0900

    02

--- main.c
+++ main.c
@@ -3,22 +3,12 @@
 
 int main(int argc, char *argv[])
 {
-    int i = 10;
-    char c = 'a';
+    int i = 300 ;
     
-    int *iptr ;
-    iptr = &i;
+    int *pi = &i;
+    char *pc = &i;
     
-    char *cptr;
-    cptr = &c;
-    
-    int *iptr2;
-    iptr2 = iptr;
-    
-    
-    printf("i : %p\n %p (size:%i)\n", iptr, &i, sizeof(iptr));
-    printf("c : %p\n %p (size:%i)\n", cptr, &c, sizeof(cptr)); //  포인터 변수 자체는 4byte 
-    printf("iptr2:%p, %i\n", iptr2, *iptr2);
+    printf("%i, %i, %i \n",i,*pi,*pc); 
   
   system("PAUSE");	
   return 0;
